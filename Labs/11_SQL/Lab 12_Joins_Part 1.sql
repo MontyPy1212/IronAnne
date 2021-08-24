4205f110c404bce6d7a6750cbeeb96d8849d4070
MontyPy1212 <anne.ihle@gmx.de>
2021-08-24 17:06:02 +0200

    LAB

--- Labs/11_SQL/Lab 12_Joins_Part 1.sql
+++ Labs/11_SQL/Lab 12_Joins_Part 1.sql
@@ -47,17 +47,17 @@ limit 1;
 #1.3 Most active customer (the customer that has rented the most number of films)
 #INNER JOIN
 SELECT 
-	customer.customer_id,
+    customer.customer_id,
     customer.first_name,
     customer.last_name,
-    count(rental.rental_id) as num_film_rentals
-FROM 
-	customer
-		INNER JOIN 
-	rental ON customer.customer_id = rental.customer_id
-group by customer.customer_id
-order by count(rental.rental_id) desc
-limit 1;
+    COUNT(rental.rental_id) AS num_film_rentals
+FROM
+    customer
+        INNER JOIN
+    rental ON customer.customer_id = rental.customer_id
+GROUP BY customer.customer_id
+ORDER BY COUNT(rental.rental_id) DESC
+LIMIT 1;
 
 #1.4 List number of films per category.
 	#confer 1.1
@@ -77,7 +77,7 @@ SELECT
     staff.staff_id,
     staff.first_name,
     staff.last_name,
-    SUM(payment.amount) AS total_amount
+    SUM(payment.amount) AS total_amount_USD
 FROM
     staff
         LEFT JOIN
@@ -87,15 +87,58 @@ WHERE
 GROUP BY staff.staff_id;
 
 #1.7 List each film and the number of actors who are listed for that film.
+SELECT 
+    film.film_id,
+    film.title,
+    COUNT(film_actor.actor_id) AS num_actors
+FROM
+    film_actor
+        LEFT JOIN
+    film ON film.film_id = film_actor.film_id
+GROUP BY film.film_id
+ORDER BY num_actors DESC;
 
+#1.8 Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name. 
 
-#1.8 Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name. Bonus: Which is the most rented film? The answer is Bucket Brotherhood This query might require using more than one join statement. Give it a try.
+SELECT 
+    customer.customer_id,
+	customer.first_name,
+    customer.last_name, 
+    SUM(payment.amount) AS total_payment
+FROM
+    payment
+        LEFT JOIN
+    customer ON customer.customer_id = payment.customer_id
+GROUP BY customer.customer_id
+ORDER BY customer.last_name ASC;
+
+#Bonus: Which is the most rented film? The answer is Bucket Brotherhood. This query might require using more than one join statement. Give it a try.
+
+SELECT 
+    f.title,
+    COUNT(r.rental_id) AS 'Times Rented'
+FROM
+    film f
+        INNER JOIN
+    inventory i ON f.film_id = i.film_id
+        INNER JOIN
+    rental r ON r.inventory_id = i.inventory_id
+GROUP BY f.title
+ORDER BY COUNT(r.rental_id) desc
+LIMIT 1;
 
 ## Part 2 
 #2.1 Write a query to display for each store its store ID, city, and country.
+
+
 #2.2 Write a query to display how much business, in dollars, each store brought in.
+
 #2.3 What is the average running time of films by category?
+
 #2.4 Which film categories are longest?
+
 #2.5 Display the most frequently rented movies in descending order.
+
 #2.6 List the top five genres in gross revenue in descending order.
+
 #2.7 Is "Academy Dinosaur" available for rent from Store 1?
